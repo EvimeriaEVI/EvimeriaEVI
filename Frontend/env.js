da06992c7cd57f8709915a5f69ea0c05c2775610
EvimeriaEVI <34174067+EvimeriaEVI@users.noreply.github.com>
2019-11-15 14:36:05 +0100

    Add files via upload

---
+++ Frontend/env.js
@@ -0,0 +1,14 @@
+window.env = {
+  api: {
+    authUrl: 'https://www.eviex.online/api/v2/barong',
+    tradeUrl: 'https://www.eviex.online/api/v2/peatio',
+    applogicUrl: 'https://www.eviex.online/api/v2/applogic',
+    rangerUrl: 'wss://www.eviex.online/api/v2/ranger',
+  },
+  minutesUntilAutoLogout: '60',
+  withCredentials: false,
+  captcha: {
+      captchaType: 'recaptcha',
+      siteKey: '6Lf4hcAUAAAAAJ-d9poAdx4XMsejgwedTi0DhxQK',
+  },
+};
